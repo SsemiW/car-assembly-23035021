b1dfb535022777fe0f2e1d91644a9f1dd696a038
SsemiW <asdfhck@naver.com>
2026-06-11 10:25:54 +0900

    isValidCheck bool 반환 타입 적용 및 비즈니스 로직 파일 분리
    - isValidCheck() 반환 타입 int → bool, 도달 불가능한 return 제거
    - assemble.h / assemble.cpp 분리
      - enum class, CarConfig, isValidCheck / runProducedCar / testProducedCar 이동
    - main.cpp 는 UI 흐름(입력·메뉴·select 함수)만 유지
    - Debug 빌드 테스트 코드에서 assemble.h include 로 isValidCheck 접근 가능

---
+++ assemble.h
@@ -0,0 +1,41 @@
+#pragma once
+
+enum class CarType
+{
+    SEDAN = 1,
+    SUV,
+    TRUCK
+};
+
+enum class Engine
+{
+    GM = 1,
+    TOYOTA,
+    WIA,
+    BROKEN
+};
+
+enum class BrakeSystem
+{
+    MANDO = 1,
+    CONTINENTAL,
+    BOSCH
+};
+
+enum class SteeringSystem
+{
+    BOSCH = 1,
+    MOBIS
+};
+
+struct CarConfig
+{
+    CarType        carType        = CarType::SEDAN;
+    Engine         engine         = Engine::GM;
+    BrakeSystem    brakeSystem    = BrakeSystem::MANDO;
+    SteeringSystem steeringSystem = SteeringSystem::BOSCH;
+};
+
+bool isValidCheck(const CarConfig& config);
+void runProducedCar(const CarConfig& config);
+void testProducedCar(const CarConfig& config);
--- main.cpp
+++ main.cpp
@@ -1,6 +1,7 @@
-﻿#ifdef _DEBUG
+#ifdef _DEBUG
 
 #include "gmock/gmock.h"
+#include "assemble.h"
 
 int main()
 {
@@ -13,6 +14,7 @@ int main()
 #include <stdio.h>
 #include <string.h>
 #include <stdlib.h>
+#include "assemble.h"
 
 #define CLEAR_SCREEN "\033[H\033[2J"
 
@@ -25,49 +27,10 @@ enum QuestionType
     Run_Test,
 };
 
-enum class CarType
-{
-    SEDAN = 1,
-    SUV,
-    TRUCK
-};
-
-enum class Engine
-{
-    GM = 1,
-    TOYOTA,
-    WIA,
-    BROKEN
-};
-
-enum class BrakeSystem
-{
-    MANDO = 1,
-    CONTINENTAL,
-    BOSCH
-};
-
-enum class SteeringSystem
-{
-    BOSCH = 1,
-    MOBIS
-};
-
-struct CarConfig
-{
-    CarType        carType        = CarType::SEDAN;
-    Engine         engine         = Engine::GM;
-    BrakeSystem    brakeSystem    = BrakeSystem::MANDO;
-    SteeringSystem steeringSystem = SteeringSystem::BOSCH;
-};
-
 void selectCarType(int answer, CarConfig& config);
 void selectEngine(int answer, CarConfig& config);
 void selectbrakeSystem(int answer, CarConfig& config);
 void selectSteeringSystem(int answer, CarConfig& config);
-void runProducedCar(const CarConfig& config);
-void testProducedCar(const CarConfig& config);
-int isValidCheck(const CarConfig& config);
 void delay(int ms);
 
 void delay(int ms)
@@ -301,111 +264,6 @@ void selectSteeringSystem(int answer, CarConfig& config)
         printf("BOSCH 조향장치를 선택하셨습니다.\n");
     if (answer == 2)
         printf("MOBIS 조향장치를 선택하셨습니다.\n");
-}
-
-int isValidCheck(const CarConfig& config)
-{
-    if (config.carType == CarType::SEDAN && config.brakeSystem == BrakeSystem::CONTINENTAL)
-    {
-        return false;
-    }
-    else if (config.carType == CarType::SUV && config.engine == Engine::TOYOTA)
-    {
-        return false;
-    }
-    else if (config.carType == CarType::TRUCK && config.engine == Engine::WIA)
-    {
-        return false;
-    }
-    else if (config.carType == CarType::TRUCK && config.brakeSystem == BrakeSystem::MANDO)
-    {
-        return false;
-    }
-    else if (config.brakeSystem == BrakeSystem::BOSCH && config.steeringSystem != SteeringSystem::BOSCH)
-    {
-        return false;
-    }
-    else
-    {
-        return true;
-    }
-    return true;
-}
-
-void runProducedCar(const CarConfig& config)
-{
-    if (isValidCheck(config) == false)
-    {
-        printf("자동차가 동작되지 않습니다\n");
-    }
-    else
-    {
-        if (config.engine == Engine::BROKEN)
-        {
-            printf("엔진이 고장나있습니다.\n");
-            printf("자동차가 움직이지 않습니다.\n");
-        }
-        else
-        {
-            if (config.carType == CarType::SEDAN)
-                printf("Car Type : Sedan\n");
-            if (config.carType == CarType::SUV)
-                printf("Car Type : SUV\n");
-            if (config.carType == CarType::TRUCK)
-                printf("Car Type : Truck\n");
-            if (config.engine == Engine::GM)
-                printf("Engine : GM\n");
-            if (config.engine == Engine::TOYOTA)
-                printf("Engine : TOYOTA\n");
-            if (config.engine == Engine::WIA)
-                printf("Engine : WIA\n");
-            if (config.brakeSystem == BrakeSystem::MANDO)
-                printf("Brake System : Mando\n");
-            if (config.brakeSystem == BrakeSystem::CONTINENTAL)
-                printf("Brake System : Continental\n");
-            if (config.brakeSystem == BrakeSystem::BOSCH)
-                printf("Brake System : Bosch\n");
-            if (config.steeringSystem == SteeringSystem::BOSCH)
-                printf("SteeringSystem : Bosch\n");
-            if (config.steeringSystem == SteeringSystem::MOBIS)
-                printf("SteeringSystem : Mobis\n");
-
-            printf("자동차가 동작됩니다.\n");
-        }
-    }
-}
-
-void testProducedCar(const CarConfig& config)
-{
-    if (config.carType == CarType::SEDAN && config.brakeSystem == BrakeSystem::CONTINENTAL)
-    {
-        printf("자동차 부품 조합 테스트 결과 : FAIL\n");
-        printf("Sedan에는 Continental제동장치 사용 불가\n");
-    }
-    else if (config.carType == CarType::SUV && config.engine == Engine::TOYOTA)
-    {
-        printf("자동차 부품 조합 테스트 결과 : FAIL\n");
-        printf("SUV에는 TOYOTA엔진 사용 불가\n");
-    }
-    else if (config.carType == CarType::TRUCK && config.engine == Engine::WIA)
-    {
-        printf("자동차 부품 조합 테스트 결과 : FAIL\n");
-        printf("Truck에는 WIA엔진 사용 불가\n");
-    }
-    else if (config.carType == CarType::TRUCK && config.brakeSystem == BrakeSystem::MANDO)
-    {
-        printf("자동차 부품 조합 테스트 결과 : FAIL\n");
-        printf("Truck에는 Mando제동장치 사용 불가\n");
-    }
-    else if (config.brakeSystem == BrakeSystem::BOSCH && config.steeringSystem != SteeringSystem::BOSCH)
-    {
-        printf("자동차 부품 조합 테스트 결과 : FAIL\n");
-        printf("Bosch제동장치에는 Bosch조향장치 이외 사용 불가\n");
-    }
-    else
-    {
-        printf("자동차 부품 조합 테스트 결과 : PASS\n");
-    }
 }
 
 #endif
